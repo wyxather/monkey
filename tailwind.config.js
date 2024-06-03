710240c0cca6298e8301935dbd4d6f3f686443da
wyxather <79754485+wyxather@users.noreply.github.com>
2024-06-03 19:24:51 +0700

    chore: Use src folder structure configurations.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,13 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-geist-mono)"],
-      },
-    },
-  },
+  theme: {},
   darkMode: "class",
   plugins: [nextui()],
-}
+};
